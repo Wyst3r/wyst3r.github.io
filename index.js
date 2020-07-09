8a879cd142878e3df2bbc93fb5f1c86eaf23c7c8
Wyst3r <wyst3r@gmail.com>
2020-07-09 15:06:26 +0200

    iOS Web MIDI browser fix

--- index.js
+++ index.js
@@ -717,28 +717,32 @@ function drawScene(currentTime) {
 }
 
 function onMIDISuccess(access) {
-    alert('midi success: ' + access.inputs.size);
-
     var iter = access.inputs.values();
-    for (var input = iter.next(); !input.done; input = iter.next()) {
-        inputs.push(input.value);
-        alert(input.name);
+    var promises = [];
+    for (var input = iter.next(); input && !input.done; input = iter.next()) {
+        promise.push(input.value.open());
     }
 
     iter = access.ouputs.values();
-    for (var output = iter.next(); !output.done; output = iter.next()) {
-        outputs.push(output.value);
+    for (var output = iter.next(); output && !output.done; output = iter.next()) {
+        promises.push(output.value.open());
     }
 
-    // for (var input of access.inputs.values()) {
-    //     input.onmidimessage = onMIDIMessage;
-    //     inputs.push(input.name);
-    // }
-    // for (var output of access.outputs.values()) {
-    //     outputs.push(output.name);
-    // }
-    
-    updateInfo();
+    Promise.all(promises).catch(function(error) { alert(error); }).then(function() {
+        alert('ports opened');
+        iter = access.inputs.values();
+        for (var input = iter.next(); input && !input.done; input = iter.next()) {
+            //inputs.push(input.name);
+            alert('input.name: ' + input.name + ', input.value.name: ' + input.value.name);
+        }
+
+        iter = access.ouputs.values();
+        for (var output = iter.next(); output && !output.done; output = iter.next()) {
+            //outputs.push(output.name);
+        }
+
+        updateInfo();
+    });
 }
 
 function onMIDIFailure(e) {
